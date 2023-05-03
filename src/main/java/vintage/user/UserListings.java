65c355c3f39017abf92b80e01d4727e84ad23fed
daniel_sp <danielsp356@gmail.com>
2023-05-03 11:47:05 +0100

    Refactor items

--- src/main/java/vintage/user/UserListings.java
+++ src/main/java/vintage/user/UserListings.java
@@ -6,11 +6,31 @@ import java.util.HashMap;
 /**
  *  Type UserList
  */
-public class UserList {
+public class UserListings {
+    private Map<String, User> users; /* ! Map of Users by Email*/
 
-    private final Map<String, User> users = new HashMap<String, User>(); /* ! Map of Users by Email*/
+    /**
+     * Creates a new UserList object.
+     */
+    public void UserListings() {
+        this.users = new HashMap<String, User>();
+    }
 
-    public Map<String, User> getUsers() { return this.users; }
+    /**
+     * Creates a new UserList object with the specified users.
+     *
+     * @param users a Map<String, User> containing the users
+     */
+    public void UserListings(Map<String, User> users) {
+        this.users = Map.copyOf(users);
+    }
+
+    /**
+     * Returns the list of Users
+     *
+     * @return Map<String, User>
+     */
+    public Map<String, User> getUsers() { return Map.copyOf(this.users); }
 
     /**
      * Adds a User to users
