9fc54ce4bd40be772a29424f160a93a16e41bb47
Mário Rodrigues <mjsrodrigues4@gmail.com>
2023-05-06 14:32:39 +0100

    Fix receipts

--- src/main/java/seeds/UserSeeder.java
+++ src/main/java/seeds/UserSeeder.java
@@ -13,10 +13,11 @@ public class UserSeeder{
         List<User> users = new ArrayList<>();
         ArrayList<Receipt> receipts = new ArrayList<>();
         for (int i = 0; i < numUsers; i++) {
+            List<BuyerReceipt> buyerReceipts = ReceiptSeeder.generateBuyerReceipts(3);
+            List<SellerReceipt> sellerReceipts = ReceiptSeeder.generateSellerReceipts(3);
+            receipts.addAll(buyerReceipts);
+            receipts.addAll(sellerReceipts);
             User user = new User("User " + i, "user" + i + "@example.com", new Address("123 Main St", "Anytown", "Anystate", "12345"), "123-45-6789", receipts);
-            ReceiptSeeder receiptSeeder = new ReceiptSeeder();
-            List<BuyerReceipt> buyerReceipts = receiptSeeder.generateBuyerReceipts(3);
-            List<SellerReceipt> sellerReceipts = receiptSeeder.generateSellerReceipts(3);
             users.add(user);
         }
         return users;
