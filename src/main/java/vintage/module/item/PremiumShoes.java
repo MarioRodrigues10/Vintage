9ca798a9885829a12c5bcb614dfd4bf116035a8d
daniel_sp <danielsp356@gmail.com>
2023-05-06 21:14:19 +0100

    Refactor for Time Observer

--- src/main/java/vintage/module/item/PremiumShoes.java
+++ src/main/java/vintage/module/item/PremiumShoes.java
@@ -1,7 +1,7 @@
 package vintage.module.item;
 
 import vintage.module.item.carrier.Carrier;
-import vintage.module.others.Time;
+import vintage.time.Time;
 import vintage.module.user.User;
 
 import java.math.BigDecimal;
