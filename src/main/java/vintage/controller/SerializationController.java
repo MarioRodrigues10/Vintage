337bbeb6033faca9e07fb30f6db4f10bb45dfa4e
Pedro Sousa <97390636+Pdf0@users.noreply.github.com>
2023-05-11 14:25:19 +0100

    Add saving (#61)

---
+++ src/main/java/vintage/controller/SerializationController.java
@@ -0,0 +1,34 @@
+package vintage.controller;
+
+
+import vintage.module.item.carrier.CarrierListings;
+import vintage.module.order.OrderListings;
+import vintage.module.user.UserListings;
+import vintage.view.SerializationView;
+
+public class SerializationController {
+
+    public static void save() {
+        String folderName = SerializationView.save();
+
+        UserListings.getInstance().saveUsers(folderName);
+        OrderListings.getInstance().saveOrders(folderName);
+        CarrierListings.getInstance().saveCarriers(folderName);
+
+        SerializationView.saved();
+
+        AdminController.menu();
+    }
+
+    public static void load() {
+        String folderName = SerializationView.load();
+
+        UserListings.getInstance().loadUsers(folderName);
+        OrderListings.getInstance().loadOrders(folderName);
+        CarrierListings.getInstance().loadCarriers(folderName);
+
+        SerializationView.loaded();
+
+        AdminController.menu();
+    }
+}
