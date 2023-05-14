e7ccae00f524a0f406a1ac71b16f8944d3806fa0
Mário Rodrigues <mjsrodrigues4@gmail.com>
2023-05-14 23:46:48 +0100

    Fix typo

--- src/main/java/vintage/view/AdminView.java
+++ src/main/java/vintage/view/AdminView.java
@@ -15,7 +15,7 @@ public class AdminView {
         Util.println  ("[7] Statistics");
         Util.println  ("[8] Back\n");
 
-        Integer option = Util.inputOption(7);
+        Integer option = Util.inputOption(8);
 
         return option;
     }
